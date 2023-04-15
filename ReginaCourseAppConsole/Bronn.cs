ae761e2a70e3b12be91807c58e4e4c329a2e0e23
AlfredMuragariefu <Muragariefu@yandex.ru>
2023-04-15 22:48:53 +0300

    Добавьте файлы проекта.

---
+++ ReginaCourseAppConsole/Bronn.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReginaCourseAppConsole
+{
+    internal class Bronn
+    {
+
+        public static void BronKerboschAlg(HashSet<int> r, List<int> p, List<int> x, int[,] graph)
+        {
+            string fileName = "output.txt";
+            HashSet<int> myset = new(r.Select(x => x + 1));
+            if (p.Count == 0 && x.Count == 0) // maximal clique found
+            {
+
+                
+                using (StreamWriter writer = File.AppendText(fileName))
+                {
+
+                    writer.Write("Maximal clique: [" + string.Join(", ", myset) + "]\n" );
+
+                }
+
+                //Console.WriteLine("Maximal clique: [" + string.Join(", ", myset) + "]");
+                return;
+            }
+
+            foreach (int v in new List<int>(p)) // make a copy for iteration
+            {
+                HashSet<int> vNeighbors = new HashSet<int>();
+                for (int i = 0; i < graph.GetLength(0); i++)
+                {
+                    if (graph[v, i] == 1)
+                        vNeighbors.Add(i);
+                }
+
+                HashSet<int> subgraph = new HashSet<int>(r.Union(vNeighbors));
+
+                if (subgraph.IsSupersetOf(r) && subgraph.IsSupersetOf(vNeighbors))
+                {
+                    HashSet<int> newR = new HashSet<int>(r);
+                    newR.Add(v);
+
+                    List<int> newP = new List<int>(p);
+                    newP.RemoveAll(w => graph[v, w] == 0);
+
+                    List<int> newX = new List<int>(x);
+                    newX.RemoveAll(w => graph[v, w] == 0);
+
+                    BronKerboschAlg(newR, newP, newX, graph);
+                    p.Remove(v);
+                    x.Add(v);
+                }
+            }
+        }
+    }
+}
---
+++ ReginaCourseAppConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using ReginaCourseAppConsole;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите путь к файлу: ");
+        string FileName = Console.ReadLine();
+        using (StreamReader sr = new StreamReader(FileName))
+        {
+            string input = sr.ReadToEnd(); // read the string from file
+            string[] rows = input.Split('\n');
+            int[,] array = new int[rows.Length, rows[0].Split(',').Length];
+
+            for (int i = 0; i < rows.Length; i++)
+            {
+                string[] columns = rows[i].Split(',');
+                for (int j = 0; j < columns.Length; j++)
+                {
+                    int value;
+                    if (int.TryParse(columns[j], out value))
+                    {
+                        array[i, j] = value;
+                    }
+                }   
+            }
+            HashSet<int> r = new HashSet<int>();
+            List<int> p = Enumerable.Range(0, array.GetLength(0)).ToList();
+            List<int> x = new List<int>();
+            string fileName = "output.txt";
+            File.Delete(fileName);
+            Bronn.BronKerboschAlg(r,p,x,array);
+        }
+    }
+}
